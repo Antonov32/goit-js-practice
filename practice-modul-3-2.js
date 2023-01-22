5ed388f2030995f6421373a1539cfde4b86778df
Antonov32 <trockiy32@icloud.com>
2023-01-22 21:33:34 +0100

    practice js spread/rest

--- practice-modul-3-2.js
+++ practice-modul-3-2.js
@@ -2,6 +2,186 @@
 console.log("Практика. Модуль 3. Занятие 2.");
 
 //
+// Операция spread (распылление)
+//
+
+// 1. Старый метод сшивания concat
+
+// const numbers = [1, 2, 3].concat([4, 5, 6], 7, 8);
+
+// console.log(numbers);
+
+// 2. Новый метод сшивания spread (...)
+
+// const numbers = [0, 5, 10, ...[1, 2, 3]];
+
+// console.log(numbers);
+
+//
+// Поиск самой маленькой или большой температуры
+//
+
+// const temps = [1, 5, 10, -5];
+
+// console.log(Math.max(...temps));
+
+//
+// Сшиваем несколько массивов в один через spread
+//
+
+// const lastWeekTemps = [1, 2, 3];
+// const currentWeekTemps = [4, 5, 6];
+// const nextWeekTemps = [7, 8, 9];
+
+// const allTemps = [...lastWeekTemps, ...currentWeekTemps, ...nextWeekTemps];
+
+// console.log(allTemps);
+
+//
+// Распыление обьектов
+// Object.prototype.assign() и spread
+//
+
+// const a = { x: 1, y: 2 };
+// const b = { x: 0, z: 3 };
+
+// const c = Object.assign({ name: "Mango" }, a, b);
+
+// console.log(c);
+
+//
+// Современный синтаксис
+//
+
+// const c = {
+//   word: "Mango",
+//   ...a,
+//   ...b,
+// };
+
+// console.log(c);
+
+//
+// Деструктуризация обьекта
+// Значения по умолчанию
+// Тия переменой отличие от имени свойства
+//
+
+// const playlist = {
+//   title: "Мой супер плейлист",
+//   rating: 5,
+//   tracks: ["track - 1", "track - 2", "track - 3"],
+//   trackCount: 3,
+// };
+
+// const { title, rating, tracks, trackCount } = playlist;
+
+// console.log(title, rating, tracks, trackCount, (author = "Michael Jackson"));
+
+// console.log(title);
+// console.log(rating);
+// console.log(tracks);
+// console.log(trackCount);
+// console.log(author);
+
+//
+// Глубокая деструктуризация
+//
+
+// const profile = {
+//   name: "Michael Jordan",
+//   tag: "MJ",
+//   place: "LA, USA",
+//   avatar: "https://www.biography.com/athlete/michael-jordan",
+//   stats: {
+//     followers: 15000000,
+//     views: 1000000,
+//     likes: 7000000,
+//   },
+// };
+
+// const {
+//   name,
+//   tag,
+//   place,
+//   avatar,
+//   stats: { followers, views, likes },
+// } = profile;
+
+// const { followers, views, likes } = stats;
+
+// console.log(name, tag, place, avatar, followers, views, likes);
+
+//
+// Деструктуризация массивов
+//
+
+// const rgb = [100, 200, 300];
+
+// const [red, green, blue] = rgb;
+
+// console.log(red);
+
+// const authors = {
+//   kiwi: 9,
+//   poly: 6,
+//   ajax: 3,
+//   mango: 1,
+// };
+
+// const entries = Object.entries(authors);
+
+// for (const entry of entries) {
+//   const [name, rating] = entry;
+//   console.log(name, rating);
+// }
+
+// //
+// // Операция rest (сбор)
+// //
+
+// const firstPerson = {
+//   name: "Michael Jordan",
+//   tag: "MJ",
+//   place: "LA, USA",
+//   avatar: "https://www.biography.com/athlete/michael-jordan",
+//   stats: {
+//     followers: 15000000,
+//     views: 1000000,
+//     likes: 7000000,
+//   },
+// };
+
+// const { name, tag, place, ...restProps } = firstPerson;
+// console.log(firstPerson);
+// console.log(restProps);
+
+//
+// Паттерн "Обьект настроек"
+// - деструктуризация параметра-обьекта в подписи функции
+// - rest при деструктаризации в подписи
+//
+
+const showProfileInfo = function (userProfile) {
+  const { name, tag, place, avatar, stats } = userProfile;
+  console.log(userProfile);
+};
+
+const profile = {
+  name: "Michael Jordan",
+  tag: "MJ",
+  place: "LA, USA",
+  avatar: "https://www.biography.com/athlete/michael-jordan",
+  stats: {
+    followers: 1555,
+    views: 1033,
+    likes: 756,
+  },
+};
+
+showProfileInfo(profile);
+
+//
 // Работаем с коллекцией товаров в корзине!
 // getItems()
 // add(product)
@@ -17,20 +197,20 @@ console.log("Практика. Модуль 3. Занятие 2.");
 // { name: '🍑', price: 110 }
 //
 
-const cart = {
-  items: [],
-  getItems() {},
-  add(product) {},
-  remove(productName) {},
-  clear() {},
-  countTotalPrice() {},
-  increaseQuantity(productName) {},
-  decreaseQuantity(productName) {},
-};
+// const cart = {
+//   items: [],
+//   getItems() {},
+//   add(product) {},
+//   remove(productName) {},
+//   clear() {},
+//   countTotalPrice() {},
+//   increaseQuantity(productName) {},
+//   decreaseQuantity(productName) {},
+// };
 
 // console.table(cart.getItems());
 
-cart.add({ name: "🍎", price: 50 });
-cart.add({ name: "🍇", price: 70 });
-cart.add({ name: "🍋", price: 60 });
-cart.add({ name: "🍑", price: 110 });
+// cart.add({ name: "🍎", price: 50 });
+// cart.add({ name: "🍇", price: 70 });
+// cart.add({ name: "🍋", price: 60 });
+// cart.add({ name: "🍑", price: 110 });
